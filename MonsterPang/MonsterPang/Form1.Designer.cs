4915439aa5f59d820595bd35cff5cea1a2e3c7f9
luvgaram <garam@nhnnext.org>
2014-06-13 11:22:25 +0900

    GUI start

--- MonsterPang/MonsterPang/Form1.Designer.cs
+++ MonsterPang/MonsterPang/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MonsterPang
 {
-    partial class Form1
+    partial class MonsterPang
     {
         /// <summary>
         /// 필수 디자이너 변수입니다.
@@ -28,12 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MonsterPang));
+            this.stones = new System.Windows.Forms.PictureBox();
+            this.monster = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.stones)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.monster)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // stones
+            // 
+            this.stones.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.stones.Image = global::MonsterPang.Properties.Resources.stones;
+            this.stones.Location = new System.Drawing.Point(0, 328);
+            this.stones.Name = "stones";
+            this.stones.Size = new System.Drawing.Size(640, 632);
+            this.stones.TabIndex = 0;
+            this.stones.TabStop = false;
+            this.stones.Paint += new System.Windows.Forms.PaintEventHandler(this.MonsterPang_Paint);
+            // 
+            // monster
+            // 
+            this.monster.Image = global::MonsterPang.Properties.Resources.monster1;
+            this.monster.Location = new System.Drawing.Point(0, 0);
+            this.monster.Name = "monster";
+            this.monster.Size = new System.Drawing.Size(640, 328);
+            this.monster.TabIndex = 1;
+            this.monster.TabStop = false;
+            this.monster.Paint += new System.Windows.Forms.PaintEventHandler(this.MonsterPang_Paint2);
+            // 
+            // MonsterPang
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(640, 765);
+            this.Controls.Add(this.stones);
+            this.Controls.Add(this.monster);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MonsterPang";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MonsterPang_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.stones)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.monster)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox stones;
+        private System.Windows.Forms.PictureBox monster;
     }
 }
 
--- MonsterPang/MonsterPang/Form1.cs
+++ MonsterPang/MonsterPang/Form1.cs
@@ -10,11 +10,39 @@ using System.Windows.Forms;
 
 namespace MonsterPang
 {
-    public partial class Form1 : Form
+    public partial class MonsterPang : Form
     {
-        public Form1()
+        private Bitmap hpBitmap = Properties.Resources.hp;
+        private Bitmap st1Bitmap = Properties.Resources.stone1;
+        private Bitmap st2Bitmap = Properties.Resources.stone2;
+        private Bitmap st3Bitmap = Properties.Resources.stone3;
+        private Bitmap st4Bitmap = Properties.Resources.stone4;
+        private Bitmap st5Bitmap = Properties.Resources.stone5;
+        private Bitmap st6Bitmap = Properties.Resources.stone6;
+
+        public MonsterPang()
         {
             InitializeComponent();
+        }
+
+        private void MonsterPang_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(st1Bitmap, 12, 6, st1Bitmap.Width, st1Bitmap.Height);
+            e.Graphics.DrawImage(st2Bitmap, 115, 6, st2Bitmap.Width, st2Bitmap.Height);
+            e.Graphics.DrawImage(st3Bitmap, 218, 6, st3Bitmap.Width, st3Bitmap.Height);
+            e.Graphics.DrawImage(st4Bitmap, 321, 6, st4Bitmap.Width, st4Bitmap.Height);
+            e.Graphics.DrawImage(st5Bitmap, 424, 6, st5Bitmap.Width, st5Bitmap.Height);
+            e.Graphics.DrawImage(st6Bitmap, 527, 6, st6Bitmap.Width, st6Bitmap.Height);
+        }
+
+        private void MonsterPang_Paint2(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(hpBitmap, 10, 270, hpBitmap.Width, hpBitmap.Height); 
+        }
+
+        private void MonsterPang_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
